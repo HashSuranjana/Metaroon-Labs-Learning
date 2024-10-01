63b8ac727a1a6a40bed67df8bc3bec33e547d4af
Hash <hasithasuranjana12@gmail.com>
2024-10-01 15:51:10 +0530

    components structures added

--- modern-bank-web/src/App.tsx
+++ modern-bank-web/src/App.tsx
@@ -1,10 +1,37 @@
 import React from "react"
+import {Navbar,Hero,Billing,Business,CardDeal,Client,Cta,GetStarted,Stats,Testomonial,Footer} from './components'
+import styles from './style'
 import './index.css'
 
 const App = () => {
   return (
     <div className=' bg-primary w-full overflow-hidden '>
-      <h1>Hello world</h1>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className= {`${styles.boxWidth}`}>
+          <Navbar/>
+        </div>
+      </div>
+
+      <div className={`${styles.flexStart} bg-primary`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero/>
+        </div>
+      </div>
+
+      <div className={`${styles.paddingX} ${styles.flexStart} bg-primary`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats/>
+          <Business/>
+          <Billing/>
+          <CardDeal/>
+          <Client/>
+          <Cta/>
+          <GetStarted/>
+          <Testomonial/>
+          <Footer/>
+        </div>
+      </div>
+
     </div>
   )
 }
---
+++ modern-bank-web/src/components/index.ts
@@ -0,0 +1,25 @@
+import Navbar from "./navbar"
+import Hero from "./hero"
+import Billing from "./billing"
+import Business from "./business"
+import CardDeal from "./cardDeal"
+import Client from "./client"
+import Cta from "./cta"
+import GetStarted from "./getStarted"
+import Stats from "./stats"
+import Testomonial from "./testomonials"
+import Footer from "./footer"
+
+export {
+    Navbar,
+    Hero,
+    Billing,
+    Business,
+    CardDeal,
+    Client,
+    Cta,
+    GetStarted,
+    Stats,
+    Testomonial,
+    Footer
+}
