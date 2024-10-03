5f4797ceb1bae0fd89992f96c51a915926e327bc
Hash <hasithasuranjana12@gmail.com>
2024-10-03 10:08:45 +0530

    Cta component done and button bug fix

--- modern-bank-web/src/components/button.tsx
+++ modern-bank-web/src/components/button.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 
-const Button = ({ styles }) => (
+interface PropStyles {
+  'styles' : string | undefined
+}
+
+
+
+const Button : React.FC<PropStyles>= ({ styles }) => (
   <button type="button" className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}>
     Get Started
   </button>
 );
+
+
 
 export default Button;
